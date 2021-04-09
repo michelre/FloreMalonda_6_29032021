d5fa3ca9344f5be1438772fa6f748691e41fce4b
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-04-09 22:56:08 +0200

    test composant bouton

---
+++ js/button.js
@@ -0,0 +1,9 @@
+export default class Button extends HTMLElement {
+
+    constructor () {
+      super()
+      this.shadow = this.attachShadow({mode: 'closed'})
+      this.shadow.innerHTML = `<style>
+    }
+}
+
