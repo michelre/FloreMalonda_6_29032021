95536bcd98fb6632f0ea347b49a6a949d7b0865d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-04-15 22:13:00 +0200

    test méthode fetch

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 import Button from './components/Button.js';
 import Tag from './components/Tag.js';
 
-
 const button = new Button();
 const tag = new Tag();
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+
+fetch(`database.json`)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
 
 document.querySelector(`#app`).innerHTML = `<div>${button.render()} ${tag.render()}</div>`
 
