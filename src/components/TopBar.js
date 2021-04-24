b8fa7e54c05549a9c6926da6148632a9a2e10b74
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-04-24 21:37:41 +0200

    test custom tags

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,13 +6,12 @@ class TopBar {
         return (`
             <div class="TopBar">
                 <header class="header" role="banner">
-                    <a aria-label="FishEye Logo" href="index.html" class="header-png"> <!--  ATTENTION INFO TEST A CHANGER -->
+                    <a aria-label="FishEye Logo" href="index.html" class="header-png">
                         <img role="img" alt="Fisheye Home page" src="public/img/logo.png"/>
                     </a>
                     <nav role="navigation" class="header-navigation" aria-label="main navigation"></nav>    
                 </header>
-        
-                <h1>Nos photographes</h1>
+
             </div>
         `)
 
