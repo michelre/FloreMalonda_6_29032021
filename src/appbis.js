b87377f38c35e08c949697a318e961009ba33963
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-04-26 22:10:29 +0200

    correction

--- src/appbis.js
+++ src/appbis.js
@@ -1,22 +1,22 @@
-// import Presentation from './components/Presentation.js';
+import Presentation from './components/Presentation.js';
 
-// function createNode(element) {
-//     return document.createElement(element);
-// }
+function createNode(element) {
+    return document.createElement(element);
+}
 
-// const div = document.querySelector('#photograph');
+const div = document.querySelector('#photograph');
 
-// fetch('database.json')
-// .then((resp) => resp.json())
-// .then(function(data) {
-//   let photograph = data.photograph;
-//   return photograph.map(function(photographer) {
-//     const presentation = new Presentation(photographer.portrait, photographer.name, photographer.city, photographer.country, photographer.tagline,photographer.price, photographer.tags);
-//     let divPresentation = createNode('div');
-//     divPresentation.innerHTML =presentation.render();
-//     div.append(divPresentation);
-//   })
-// })
-// .catch(function(error) {
-//   console.log(error);
-// });
+fetch('database.json')
+.then((resp) => resp.json())
+.then(function(data) {
+  let photograph = data.photograph;
+  return photograph.map(function(photographer) {
+    const presentation = new Presentation(photographer.portrait, photographer.name, photographer.city, photographer.country, photographer.tagline,photographer.price, photographer.tags);
+    let divPresentation = createNode('div');
+    divPresentation.innerHTML =presentation.render();
+    div.append(divPresentation);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,46 +1,43 @@
-// import Tag from './Tag.js';
-// // import Button from '../Button.js';
+import Tag from './Tag.js';
+import Button from './Button.js';
 
 
-// class Presentation  {
+class Presentation  {
 
-//     constructor (portrait, name, city,country, tagline, price, tags) {
-//         this.portrait=portrait
-//         this.name=name
-//         this.city=city
-//         this.country=country
-//         this.tagline=tagline
-//         this.price=price
-//         this.tags=tags
-//     } 
+    constructor (portrait, name, city,country, tagline, price, tags) {
+        this.portrait=portrait
+        this.name=name
+        this.city=city
+        this.country=country
+        this.tagline=tagline
+        this.price=price
+        this.tags=tags
+    } 
     
-//     render() {
-//         const tag = new Tag(this.tags);
-//         return (`
-//             <div>
-//                 <div class="gp_img_title_avatar_page">
-//                     <div class ="img_avatar_page">
-//                         <img src="../../public/img/photographers/${this.portrait}" alt="portrait">
-//                     </div>
-//                     <div class ="title_avatar_page">
-//                         <h2>${this.name}</h2>
-//                     </div>
-//                 </div>
-//                 <div class="gp_loc_tln_pr_tg_avatar_page">
-//                     <div class ="localisation_avatar_page">
-//                         <p>${this.city}, ${this.country}</p>
-//                     </div>
-//                     <div class ="tagline_avatar_home_page">
-//                         <p>${this.tagline}</p>
-//                     </div>
-//                     <div class ="price_avatar_home_page">
-//                         <p>${this.price} €</p>
-//                     </div>
-//                     <div class ="tag_appli">${tag.render()}</div>
-//                 </div>
-//             </div>
-//         `);
-//     }
-// }
+    render() {
+        const tag = new Tag(this.tags);
+        const button = new Button();
+        return (`
+            <div>
+                <div class="gp_presentation">
+                    <div class="title_avatar_page">
+                        <h2>${this.name}</h2>
+                    </div>
+                    <div class="localisation_avatar_page">
+                        <p>${this.city}, ${this.country}</p>
+                    </div>
+                    <div class="tagline_avatar_home_page">
+                        <p>${this.tagline}</p>
+                    </div>
+                    <div class="tag_appli">${tag.render()}</div>
+                </div>
+                <div class="button_contact>${button.render()}</div>
+                <div class="img_avatar_page">
+                    <img src="../../public/img/photographers/${this.portrait}" alt="portrait">
+                </div>
+            </div>
+        `);
+    }
+}
 
-// export default Presentation;
+export default Presentation;
