d41d13ade1bc419be27ef0c640a273245a678ad2
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-05-02 21:40:06 +0200

    custom card + test filter method

--- src/profil.js
+++ src/profil.js
@@ -1,6 +1,6 @@
 import TopBar from './components/TopBar.js';
 import Profil from './components/Profil.js';
-// import Card from './components/Card.js';
+import Card from './components/Card.js';
 
 const topbar = new TopBar();
 
@@ -22,14 +22,22 @@ fetch('database.json')
 
     const profil = new Profil(photographer.portrait, photographer.name, photographer.city, photographer.country, photographer.tagline, photographer.tags);
     let divProfil = createNode('div');
-    divProfil.innerHTML =profil.render();
+    divProfil.innerHTML = profil.render();
     div.append(divProfil);
 
-    // const media = data.media
+    data.media.filter( media => media.photograph.id === parseInt(id)).map(function(media) {
 
-     // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+      const card = new Card(media.image, media.likes);
+      let divCard = createNode('div');
+      divCard.innerHTML = card.render();
+      div.append(divCard);
+  
+    })
+
   })
+
 })
+
 
 .catch(function(error) {
   console.log(error);
