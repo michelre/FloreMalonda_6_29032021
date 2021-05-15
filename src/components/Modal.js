ef89bce7e98e4af5966691059c064f0bceb53bfb
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-05-15 18:56:43 +0200

    gestion modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,149 +1,3 @@
-// // ----------------------- DOM Elements ----------------------------
-// //modal elements
-// const modalbg = document.querySelector(".bground"); 
-// const modalBtn = document.querySelectorAll(".modal-btn"); 
-// const closeModalBtn = document.querySelectorAll(".close") 
-// // const confirmationCloseBtn = document.querySelector("#btn-closed"); 
-
-// // button submit
-// // const formValid = document.querySelector("#btn-submit");
-
-// // valid
-// const firstName = document.querySelector("#first");
-// const lastName = document.querySelector("#last");
-// const eMail = document.querySelector("#email");
-// const info = document.querySelector("#info");
-// // invalid
-// const errorFirstName = document.querySelector("#missfirst");
-// const errorLastName = document.querySelector("#misslast");
-// const errorMail = document.querySelector("#missemail");
-// const errorInfo = document.querySelector("#missinfo");
-// const confirmationValidation = document.querySelector("#confirm-modal");
-
-// // submit
-
-// const form = document.querySelector('form[name="contact"]')
-
-// // ---------- Launch Modal Form ---------
-
-// // launch modal event
-// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// // launch modal form
-// function launchModal() {
-//     modalbg.style.display = 'block';
-//     confirmationValidation.style.display = 'none';
-// }
-
-// // ---- Close Modal Form ---------
-
-// closeModalBtn[0].addEventListener("click", closeModal);
-
-// function closeModal() {
-//     modalbg.style.display = 'none';
-//     form.style.display = 'block';
-//     confirmationValidation.style.display = 'none';
-// }
-
-// // ------------ Envoi du formulaire d'inscription ------------------
-
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     validate();
-// });
-
-
-// // ------------------- validation formulaire par input -----------
-
-// function validateFirstName(firstName) {
-//   if (firstName.value.toString().trim().length < 2) {
-//       errorFirstName.style.display = "inline";
-//       errorFirstName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
-//       errorFirstName.style.color = 'red';
-//       errorFirstName.style.fontSize = '0.8rem';
-//       errorFirstName.style.marginTop = '10px';
-//       firstName.style.border = 'solid red 2px';
-//       return false;
-//   } else {
-//       errorFirstName.style.display = 'none';
-//       firstName.style.border = 'solid #279e7a 3px';
-//       return true; 
-//     }
-// }
-
-// function validateLastName(lastName) {
-//   if (lastName.value.toString().trim().length < 2) {
-//       errorLastName.style.display = 'inline';
-//       errorLastName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
-//       errorLastName.style.color = 'red';
-//       errorLastName.style.fontSize = '0.8rem';
-//       errorLastName.style.marginTop = '10px';
-//       lastName.style.border = 'solid red 2px';
-//       return false;
-//   } else {
-//       errorLastName.style.display = 'none';
-//       lastName.style.border = 'solid #279e7a 3px';
-//       return true;
-//     }
-// }
-
-// function validateEmail(eMail) {
-//     if (!/[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(eMail.value)) {
-//         errorMail.style.display = "inline"
-//         errorMail.innerText = "Veuillez entrer une adresse mail valide.";
-//         errorMail.style.color = 'red';
-//         errorMail.style.fontSize = '0.8rem';
-//         errorMail.style.marginTop = '10px';
-//         eMail.style.border = 'solid red 2px';
-//         return false;
-//     } else {
-//         errorMail.style.display = 'none';
-//         eMail.style.border = 'solid #279e7a 3px';
-//         return true;
-//     }
-// }
-
-// function validateInfo(info) {
-//   if (info.value.toString().trim().length < 2) {
-//       errorInfo.style.display = 'inline';
-//       errorInfo.innerText = "Veuillez rédiger votre message";
-//       errorInfo.style.color = 'red';
-//       errorInfo.style.fontSize = '0.8rem';
-//       errorInfo.style.marginTop = '10px';
-//       info.style.border = 'solid red 2px';
-//       return false;
-//   } else {
-//       errorInfo.style.display = 'none';
-//       info.style.border = 'solid #279e7a 3px';
-//       return true;
-//     }
-// }
-
-
-// // -------------------- Validation du formulaire ----------------------
-
-// function validate() {
-//   let isFormValidate = [];
-
-//   isFormValidate.push(validateFirstName(firstName));
-//   isFormValidate.push(validateLastName(lastName));
-//   isFormValidate.push(validateEmail(eMail));
-//   isFormValidate.push(validateInfo(info));
-
-//   if (!isFormValidate.includes(false)) {
-//       form.style.display = 'none';
-//       confirmationValidation.style.display = 'flex';
-//   }
-// }
-
-// // ------- Fermer le formulaire avec le message de validation ---------
-
-// document.querySelector("#btn-closed").addEventListener("click", closeModal);
-
-
-// ------------------------------------------------------------------------
-
-
 class Modal {
 
     constructor (name, closeModal) {
@@ -174,19 +28,27 @@ class Modal {
         })
 
         return (`
-            
-            <main>
+            <div class="bground"> 
+                <div class="content">
+                    <div 
+                        id="dialog"
+                        role="dialog" 
+                        aria-labelledby="dialog-title" 
+                        aria-describedby="dialog-desc"
+                        aria-modal="true"
+                        aria-hidden="true"
+                        tabindex="-1" 
+                        class="c-dialog"
+                    >
 
-                <div class="bground"> 
-                     <div class="content">
-                        <div class="button-block-closed">
-                            ${modalBtnClose.outerHTML}
-                        </div>
-                        
-                        <div class="modal-body">
-            
-                            <form name="contact" action="profil.html" method="post">
-
+                        <div role="document" class="c-dialog__box">
+                            <div class="button-block-closed">
+                                ${modalBtnClose.outerHTML}
+                            </div>
+                            <h2 id="dialog-title">Ma fenêtre modale</h2>
+                            <p id="dialog-desc">Je suis une fenêtre modale accessible.</p>
+                            
+                            <form action="" method="post">
                                 <h3 class="title-modal">Contactez-moi ${this.name}</h3>
 
                                 <div class="formData" id="formDataFirstName">
@@ -241,10 +103,76 @@ class Modal {
                         </div>
                     </div>
                 </div>
-            </main>
+            </div>
+               
         `)
     }
 }
 
 export default Modal;
+
+{/* <div class="bground"> 
+<div class="content">
+<div class="button-block-closed">
+    ${modalBtnClose.outerHTML}
+</div>
+
+<div class="modal-body">
+
+    <form name="contact" action="profil.html" method="post">
+
+        <h3 class="title-modal">Contactez-moi ${this.name}</h3>
+
+        <div class="formData" id="formDataFirstName">
+            <label>Prénom</label><br> 
+            <input
+            class="text-control"
+            type="text"
+            id="first"
+            name="first"
+            placeholder="Jane"
+            /><span id="missfirst"></span>
+            <br>
+        </div>
+
+        <div class="formData">
+            <label>Nom</label><br> 
+            <input
+            class="text-control"
+            type="text"
+            id="last"
+            name="last"
+            placeholder="Smith"
+            /><span id="misslast"></span>
+            <br>
+        </div>
+
+        <div class="formData">
+            <label>E-mail</label><br> 
+            <input
+            class="text-control"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="jane.smith@gmail.com"
+            /><span id="missemail"></span>
+            <br>
+        </div>
+
+        <div class="formData">
+            <label>Votre message</label><br> 
+            <textarea 
+            id="message" 
+            name="message" 
+            placeholder="Rédigez votre message">
+            </textarea>
+        </div>
+
+        <div class="button-block-valid">
+            ${modalBtnSubmit.outerHTML}
+        </div> 
+    </form>
+</div>
+</div>
+</div> */}
 
--- src/profil.js
+++ src/profil.js
@@ -1,27 +1,38 @@
+// ------------------ Components importation -------------------------
+
 import TopBar from './components/TopBar.js';
 import Profil from './components/Profil.js';
 import Card from './components/Card.js';
 import InfoBlock from './components/InfoBlock.js';
 import Modal from './components/Modal.js';
 
-
 const topbar = new TopBar();
 
+// ------------- DOM -----------------------------
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
+const div = document.querySelector('#photograph');
 
 function createNode(element) {
   return document.createElement(element);
 }
 
-// -------------------------- open Modal ------------
+// ---------------- Modal ------------
+
+// launch modal form
+
+const modalbg = document.querySelector(".bground"); 
+const form = document.querySelector('form');
 
 const openModal = function () {
+  modalbg.style.display = 'block';
   console.log("openModal")
 }
 
 const closeModal = function () {
+  modalbg.style.display = 'none';
+  form.style.display = 'block';
   console.log("closeModal")
 }
 
@@ -29,13 +40,23 @@ const submitForm = function () {
   console.log("submitForm")
 }
 
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   console.log('Nom:', e.target.last_name.value)
+//   console.log('Prénom:', e.target.first_name.value)
+//   console.log('Email:', e.target.email.value)
+//   console.log('Message:', e.target.message.value)
+// });
+
+
+
+// ------------------------------------------------------------------------
+
 document.querySelector(`#app`).innerHTML = `
   <div class="container-profil-view>
     <div class="header">${topbar.render()}</div>
   </div>
 `
-
-const div = document.querySelector('#photograph');
 
 fetch('database.json')
 .then((resp) => resp.json())
