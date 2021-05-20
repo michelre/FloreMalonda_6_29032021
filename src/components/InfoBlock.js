ccbc8940584b5d885e38ea3e62dd11bd3c3028da
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-05-20 15:12:33 +0200

    correction affichage

--- src/components/InfoBlock.js
+++ src/components/InfoBlock.js
@@ -10,7 +10,12 @@ class InfoBlock {
             <div class='container-info'>
                 <div class='price-info'>
                     <div class='info-likes'>
-                    <p>${this.sumlikes}</p><i class="fas fa-heart"></i>
+                        <div>
+                            <p>${this.sumlikes}</p>
+                        </div>
+                        <div>
+                            <i class="fas fa-heart"></i>
+                        </div>
                     </div>
                     <div>
                         <p>${this.price} /jour</p>
