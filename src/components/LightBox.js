2d4c621af32565e9214a10d85921d77f57a8217d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-05-27 18:03:53 +0200

    test

--- src/components/LightBox.js
+++ src/components/LightBox.js
@@ -57,9 +57,7 @@ class LightBox {
                         ${lightboxBtnClose.outerHTML}
                     </div>
                     <div class="slideshow-container">
-                        <div class="slideshow-img>
-                            ${mediaHtml}
-                        </div>
+                        ${mediaHtml}
                     </div>
                     <div class=lightbox-controls>
                         <div class="button-lightboxnext">
@@ -77,4 +75,46 @@ class LightBox {
 }
 
 export default LightBox; 
+
+
+// export default function Slideshow(props){
+
+//     const [slideIdx, setSlideIdx] = useState(0);
+//     const imgSize = () => {
+//         const slideshowImg = document.querySelector('.slideshow-container img');
+//         if(!slideshowImg){
+//             return 0;
+//         }
+//         return slideshowImg.width;
+//     }
+
+//     const onNext = () => {
+//         if(slideIdx === props.img.length - 1){
+//             setSlideIdx(0)
+//         } else {
+//             setSlideIdx(slideIdx + 1)
+//         }
+//     }
+
+//     const onPrev = () => {
+//         if(slideIdx === 0){
+//             setSlideIdx(props.img.length - 1)
+//         } else {
+//             setSlideIdx(slideIdx - 1)
+//         }
+//     }
+
+//     return (
+//         <div className="slideshow">
+//             <div className="slideshow-container" style={{transform: `translateX(-${slideIdx * imgSize()}px)`}}>
+//             {props.img.map((picture) => < img className='slideshow-container-img' src={picture} key={picture}/>)}
+//             </div>
+//             <div className={'slideshow-controls'}>
+//                 <img src="../chevron-left.png" className={'chevron'} onClick={onPrev}/>
+//                 <img src="../chevron-right.png" className={'chevron'} onClick={onNext}/>
+//             </div>
+//             <div className={'slideshow-idx'}>{slideIdx + 1} / {props.img.length}</div>
+//         </div>
+//     );
+// }
 
