1eb4ffaadb97e3b8732082a8a176d940c26942bd
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-06-01 18:51:33 +0200

    test

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -18,27 +18,29 @@ class Avatar  {
         const tag = new Tag(this.tags);
         return (`
             <div class="avatar_gp"> 
-                <div class="gp_img_title_avatar">
-                    <a aria-label="link page" href="profil.html?id=${this.id}">
-                        <div class="img_avatar_home">
-                            <img src="../../public/img/photographers/${this.portrait}" alt="logo">
+                <div class="compo_avatar">
+                    <div class="gp_img_title_avatar">
+                        <a aria-label="link page" href="profil.html?id=${this.id}">
+                            <div class="img_avatar_home">
+                                <img src="../../public/img/photographers/${this.portrait}" alt="logo">
+                            </div>
+                            <div class="title_avatar_home">
+                                <h2>${this.name}</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gp_loc_tln_pr_tg_avatar">
+                        <div class="localisation_avatar_home">
+                            <p>${this.city}, ${this.country}</p>
                         </div>
-                        <div class="title_avatar_home">
-                            <h2>${this.name}</h2>
+                        <div class="tagline_avatar_home">
+                            <p>${this.tagline}</p>
                         </div>
-                    </a>
-                </div>
-                <div class="gp_loc_tln_pr_tg_avatar">
-                    <div class="localisation_avatar_home">
-                        <p>${this.city}, ${this.country}</p>
+                        <div class="price_avatar_home">
+                            <p>${this.price} €/jour</p>
+                        </div>
+                        <div class="tag_appli">${tag.render()}</div>
                     </div>
-                    <div class="tagline_avatar_home">
-                        <p>${this.tagline}</p>
-                    </div>
-                    <div class="price_avatar_home">
-                        <p>${this.price} €/jour</p>
-                    </div>
-                    <div class="tag_appli">${tag.render()}</div>
                 </div>
             </div>
         `);
