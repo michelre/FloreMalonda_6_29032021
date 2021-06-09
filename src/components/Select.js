607a18eafcf6603c303afa68a63c8487b083d97a
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-06-09 22:39:29 +0200

    custom & select

--- src/components/Select.js
+++ src/components/Select.js
@@ -5,9 +5,19 @@ class Select {
     render() {
 
         return (`
-            <div>
-                <p>Trier par : </p>
+            <div class="select_container">
+                <div>
+                    <p>Trier par : </p>
+                </div>
+                <div>
+                    <select>
+                        <option value="popularity">Popularité</option>
+                        <option value="date">Date</option>
+                        <option value="title">Titre</option>
+                    </select">
+                </div>
             </div>
+            
 
         `);
     }
