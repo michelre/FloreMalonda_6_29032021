f42843bd6b2721ff100143d6a523654e19b3212d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-06-23 17:30:24 +0200

    select custom (en cours)

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,29 @@
 class Select {
 
     constructor() {
-        // this.openSelect = openSelect
+        // document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
+//     this.querySelector('.custom-select').classList.toggle('open');
+// })
+for (const dropdown of document.querySelectorAll(".custom-select-wrapper")) {
+    dropdown.addEventListener('click', function() {
+        this.querySelector('.custom-select').classList.toggle('open');
+    })
+}
+
+// window.addEventListener('click', function (e) {
+//     const select = document.querySelector('.custom-select')
+//     if (!select.contains(e.target)) {
+//         select.classList.remove('open');
+//     }
+// });
+
+window.addEventListener('click', function(e) {
+    for (const select of document.querySelectorAll('.custom-select')) {
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    }
+});
     }
 
     render() {
@@ -14,9 +36,7 @@ class Select {
                 <div class="custom-select-wrapper">
                     <div class="custom-select">
                         <div class="custom-select__trigger"><span>Popularité</span>
-                            <div class="arrow_angleup">
-                                <i class="fas fa-angle-up"></i>
-                            </div>
+                            <div class="arrow"></div>
                         </div>
                         <div class="custom-options">
                             <span class="custom-option selected" data-value="popularity">Popularité</span>
@@ -24,10 +44,8 @@ class Select {
                             <span class="custom-option" data-value="title">Titre</span>
                         </div>
                     </div>
-                </div>
+                 </div>
             </div>
-            
-
         `);
     }
 }
